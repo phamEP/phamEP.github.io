b277675f4255267f39e4f370df35f999feb8749f
ericphamEP <EricPham@cmail.carleton.ca>
2023-05-30 16:26:01 -0400

    Improve typing animations

--- js/effect.js
+++ js/effect.js
@@ -1,33 +1,43 @@
 function typing() {
-    if (line) {
-        typedBox.innerHTML = typedBox.innerHTML.slice(0, -1);
-        line = 0;
-    } else {
-        typedBox.innerHTML += '|';
-        line = 1;
-    }
-    setTimeout(typing, 500);
-}
+  // Continuous typing in README section
+  let typedBox = document.getElementById("typing");
+  let line = 0;
+  setInterval(animateTyping, 500);
 
-function typeWriter() {
-  if (i < txt.length) {
-    temp = temp.slice(0, -1);
-    temp += txt.charAt(i)+"_";
-    main.innerHTML = temp;
-    i++;
-    setTimeout(typeWriter, 250);
+  function animateTyping() {
+    if (line) {
+      typedBox.innerHTML = typedBox.innerHTML.slice(0, -1);
+      line = 0;
+    } else {
+      typedBox.innerHTML += '|';
+      line = 1;
+    }
   }
 }
 
-// Typewriter effect
-let main = document.getElementById("typewriter");
-let txt = ">"+main.innerHTML;
-let temp = "_";
-let i = 0;
 
-main.innerHTML = temp;
-setTimeout(typeWriter, 200);
+function typeWriter() {
+  // Typewriter effect
+  let main = document.getElementById("typewriter");
+  let txt = ">"+main.innerHTML;
+  let temp = "_";
+  let i = 0;
 
-let typedBox = document.getElementById("typing");
-let line = 0;
-setTimeout(typing, 200);
+  main.innerHTML = temp;
+
+  const id = setInterval(animateTerminal, 200);
+  function animateTerminal() {
+    if (i < txt.length) {
+      temp = temp.slice(0, -1);
+      temp += txt.charAt(i)+"_";
+      main.innerHTML = temp;
+      i++;
+    } else {
+      clearInterval(id);
+    }
+  }
+}
+
+
+typeWriter();
+typing();
